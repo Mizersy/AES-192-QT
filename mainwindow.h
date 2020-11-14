b6a0958c49ce58bcc015ecdd72e73fbb59cc74f8
mizersy <mizersy@163.com>
2020-11-14 13:05:30 +0800

    1.0

---
+++ mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+    void generate_key();
+    QString AES(QString textIn,QString textKey);
+    void generate_T(int C);
+    void copy_c(int C);
+    int myPow(int x,int y);
+    void ARK(int round);
+    void BS();
+    void SR();
+    void MC();
+
+private:
+    Ui::MainWindow *ui;
+};
+#endif // MAINWINDOW_H
